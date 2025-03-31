e9be57e2b6c1b042398ccda38262f09fc52a0d48
Rapli Kunz <raffikun1203@gmail.com>
2025-03-31 14:22:18 +0000

    syscall: Increase fake uname to 6.6.40

--- kernel/sys.c
+++ kernel/sys.c
@@ -1195,8 +1195,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (current_uid().val == 0 && 
 		(!strncmp(current->comm, "bpfloader", 9) ||
 		!strncmp(current->comm, "netbpfload", 10) ||
-	    	!strncmp(current->comm, "netd", 4))) {
-		strcpy(tmp.release, "5.4.186");
+	    !strncmp(current->comm, "netd", 4))) {
+		strcpy(tmp.release, "6.6.40");
 		pr_info("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
